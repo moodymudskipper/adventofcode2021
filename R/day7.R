257741079d39a6c97430edd781cc920d2b6743d3
Antoine Fabri <antoine.fabri@gmail.com>
2021-12-07 10:21:35 +0100

    day7 simplified

--- R/day7.R
+++ R/day7.R
@@ -7,15 +7,14 @@ day7 <- function() {
   input <- scan(file, what = numeric(), sep = ",", quiet = TRUE)
   counts <- transform(as.data.frame(table(input), stringsAsFactors = FALSE),
                       input = as.numeric(input))
-
-  # part1
   grid <- merge(seq(max(input)), counts, by = NULL) |>
     transform(dist = abs((x - input)))
+
+  # part1
   part1 <- min(with(grid, tapply(dist * Freq, x, sum)))
 
   # part2
-  grid$dist2 <- grid$dist * (grid$dist+1) / 2
-  part2 <- min(with(grid, tapply(dist2 * Freq, x, sum)))
+  part2 <- min(with(grid, tapply(dist * (dist+1) / 2 * Freq , x, sum)))
 
   list(part1 = part1, part2 = part2)
 }
