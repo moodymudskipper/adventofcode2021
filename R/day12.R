e00dc1e748c51fc0c004f12f89c371979f5fffaf
Antoine Fabri <antoine.fabri@gmail.com>
2021-12-12 11:48:31 +0100

    day12

---
+++ R/day12.R
@@ -0,0 +1,32 @@
+#' day 11
+#'
+#' @export
+day12 <- function() {
+  # data
+  file <- system.file("extdata/day12.txt", package = "adventofcode2021")
+  input <- unglue::unglue_data(readLines(file), "{from}-{to}")
+  # paths go both ways
+  input <-
+    rbind(input, setNames(input, c("to", "from"))) |>
+    subset(to != "start")
+  edges <- with(input, split(to, from))
+  nodes <- with(input, unique(c(from, to)))
+  visited    <- setNames(rep(FALSE, length(nodes)), nodes)
+  visited[nodes == toupper(nodes)] <- NA
+
+  rec <- function(node, edges, visited, extra) {
+    if(isTRUE(visited[node])) {
+      if (!extra) return(0)
+      visited[node] <- FALSE
+      extra <- FALSE
+    }
+    if(node == "end") return(1)
+    visited[node] <- !visited[node]
+    lapply(edges[[node]], rec, edges, visited, extra)
+  }
+  solve <- function(extra) sum(unlist(rec("start", edges, visited, extra)))
+  part1 <- solve(extra = FALSE)
+  part2 <- solve(extra = TRUE)
+
+  list(part1 = part1, part2 = part2)
+}
